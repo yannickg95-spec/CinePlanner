6125f5aef58dcbffbce747ddf0223481e0836eeb
YannickGiraud <yannickg95@gmail.com>
2025-12-15 14:25:52 +0100

    Initial Commit

---
+++ CineStager 2/CineStager_2App.swift
@@ -0,0 +1,17 @@
+//
+//  CineStager_2App.swift
+//  CineStager 2
+//
+//  Created by Yannick Giraud on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CineStager_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
